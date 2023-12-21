073ca9f11f997ebf791254dd1e1f2924006a9770
Doug Stowell <doug.stowell@gmail.com>
2023-12-21 16:00:59 +0000

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://todo_example.com"
+  site: "https://nastro-app.netlify.app",
+  integrations: [preact()]
 });
